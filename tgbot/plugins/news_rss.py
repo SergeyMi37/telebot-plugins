0dc3d8d182a59c41e5bc1104a4d6cb2ea362be4e
SergeyMi37 <sergey.mihaylenko@gmail.com>
2025-05-28 15:53:35 +0300

    add plugin rss

---
+++ tgbot/plugins/news_rss.py
@@ -0,0 +1,9 @@
+# Plugin Новости из ленты rss
+# Name Plugin: NEWS
+
+from dtb.settings import get_plugins
+from dtb.settings import logger
+
+# Добавить проверку на роль ''
+plugin_news = get_plugins('').get('NEWS')
+
